de92fc9d0c1ef2dafd0c3a1aeb014489c7fd618e
qixiaohui <qixiaohuihaha@gmail.com>
2016-06-14 23:12:34 -0700

    fix issue

--- app/src/main/java/com/infi/overwatch/overwatchandroid/Activity/HeroDetailActivity.java
+++ app/src/main/java/com/infi/overwatch/overwatchandroid/Activity/HeroDetailActivity.java
@@ -2,17 +2,24 @@ package com.infi.overwatch.overwatchandroid.Activity;
 
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -25,6 +32,8 @@ import com.infi.overwatch.overwatchandroid.Dao.Gateway;
 import com.infi.overwatch.overwatchandroid.Dao.RestClient;
 import com.infi.overwatch.overwatchandroid.R;
 import com.infi.overwatch.overwatchandroid.databinding.ActivityHeroDetailBinding;
+import com.infi.overwatch.overwatchandroid.databinding.DialogAbilitiesBinding;
+import com.infi.overwatch.overwatchandroid.model.heroDetail.Abilities;
 import com.infi.overwatch.overwatchandroid.model.heroDetail.HeroDetail;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
@@ -46,6 +55,8 @@ public class HeroDetailActivity extends AppCompatActivity {
     private TextView bioDescription;
     private ExpandableRelativeLayout expandableDescriptionRelativeLayout;
     private RelativeLayout descriptionBtn;
+    private RelativeLayout abilitiesBtn;
+    private HeroDetail heroDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +68,7 @@ public class HeroDetailActivity extends AppCompatActivity {
         heroName = getIntent().getStringExtra(HERO_ID);
         expandableDescriptionRelativeLayout = activityHeroDetailBinding.description;
         descriptionBtn = activityHeroDetailBinding.descriptionButton;
+        abilitiesBtn = activityHeroDetailBinding.abilitiesBtn;
         toolbar.setTitle(heroName);
         toolbar.setNavigationIcon(R.drawable.ic_action_back);
         setSupportActionBar(toolbar);
@@ -81,6 +93,29 @@ public class HeroDetailActivity extends AppCompatActivity {
             }
         });
 
+        abilitiesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buildDialog();
+            }
+        });
+    }
+
+    private void buildDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(HeroDetailActivity.this);
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        AbilitiesChart abilitiesChart = new AbilitiesChart(this);
+        abilitiesChart.bindData(heroDetail.getAbilities(), alertDialog);
+        alertDialog.setView(abilitiesChart);
+        alertDialog.show();
+        WindowManager.LayoutParams params = alertDialog.getWindow().getAttributes();
+        alertDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        params.dimAmount = 0.7f;
+        params.gravity = Gravity.CENTER;
+        params.width = (int) getResources().getDimension(R.dimen.dialogWidth);
+
+        alertDialog.getWindow().setAttributes(params);
     }
 
     private void init(){
@@ -88,10 +123,11 @@ public class HeroDetailActivity extends AppCompatActivity {
         gateway.getHeroDetail(heroName.toLowerCase()).enqueue(new Callback<HeroDetail>() {
             @Override
             public void onResponse(Call<HeroDetail> call, Response<HeroDetail> response) {
-                activityHeroDetailBinding.setData(response.body());
-                Picasso.with(HeroDetailActivity.this).load(response.body().getPoster()).into(detailContainer);
+                heroDetail = response.body();
+                activityHeroDetailBinding.setData(heroDetail);
+                Picasso.with(HeroDetailActivity.this).load(heroDetail.getPoster()).into(detailContainer);
                 String description = "";
-                for(String section: response.body().getDescription()){
+                for(String section: heroDetail.getDescription()){
                     description+=section;
                 }
                 bioDescription.setText(description);
@@ -121,5 +157,32 @@ public class HeroDetailActivity extends AppCompatActivity {
         objectAnimator.setDuration(300);
         objectAnimator.setInterpolator(Utils.createInterpolator(Utils.LINEAR_INTERPOLATOR));
         return objectAnimator;
+    }
+
+    private class AbilitiesChart extends LinearLayout{
+        private ImageView close;
+        public AbilitiesChart(Context context){
+            this(context, null);
+        }
+
+        public AbilitiesChart(Context context, AttributeSet attrs){
+            this(context, attrs, 0);
+        }
+
+        public AbilitiesChart(Context context, AttributeSet attrs, int defStyle){
+            super(context, attrs, defStyle);
+        }
+
+        public void bindData(Abilities abilities, final Dialog dialog){
+            DialogAbilitiesBinding dialogAbilitiesBinding = DataBindingUtil.setContentView(HeroDetailActivity.this, R.layout.dialog_abilities);
+            dialogAbilitiesBinding.setData(abilities);
+            close = dialogAbilitiesBinding.close;
+            close.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+        }
     }
 }
